f1e3c31a299f82b41273c6fea6c7217576fefd1b
sheikh Nayan <sheikhnayan1997@gmail.com>
2025-11-14 05:01:25 +0600

    test

--- app/Http/Controllers/WebsiteController.php
+++ app/Http/Controllers/WebsiteController.php
@@ -277,7 +277,6 @@ class WebsiteController extends Controller
             // dd('r');
             foreach ($request->payment_logos as $logoData) {
                 if (!empty($logoData['name'])) {
-                    dd('ss');
                     // Check if this is an existing logo (has ID) or new one
                     if (isset($logoData['id']) && $logoData['id']) {
                         // Update existing logo
@@ -304,6 +303,7 @@ class WebsiteController extends Controller
                             $paymentLogo->save();
                         }
                     } else {
+                        dd('ss');
                         // Create new logo
                         $paymentLogo = new PaymentLogo();
                         $paymentLogo->website_id = $add->id;
