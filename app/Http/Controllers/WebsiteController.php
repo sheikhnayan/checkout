147c40dbc288ae100a54da13ebbdf9ff53ea4980
sheikh Nayan <sheikhnayan1997@gmail.com>
2025-11-14 04:56:53 +0600

    test

--- app/Http/Controllers/WebsiteController.php
+++ app/Http/Controllers/WebsiteController.php
@@ -273,7 +273,9 @@ class WebsiteController extends Controller
         $smtp->save();
 
         // Handle payment logos
+        dd('s');
         if ($request->has('payment_logos')) {
+            dd('r');
             foreach ($request->payment_logos as $logoData) {
                 if (!empty($logoData['name'])) {
                     // Check if this is an existing logo (has ID) or new one
