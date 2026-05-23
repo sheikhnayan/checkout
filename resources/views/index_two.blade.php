b696dc27405bc0de5ec2dd20a2f9c6a6d40d0467
sheikh Nayan <sheikhnayan1997@gmail.com>
2026-05-23 07:10:38 +0600

    test

--- resources/views/index_two.blade.php
+++ resources/views/index_two.blade.php
@@ -5458,7 +5458,7 @@
                             @else
                                 <div class="cv-access-card is-active" data-name="package">
                                     @if(!empty($data->package_tab_ribbon))
-                                        <span class="cv-ac-ribbon">{{ $data->package_tab_ribbon }}</span>
+                                        <span class="cv-ac-ribbon" style="color: rgb(255, 255, 255) !important;">{{ $data->package_tab_ribbon }}</span>
                                     @endif
                                     <span class="cv-ac-shimmer" aria-hidden="true"></span>
                                     <span class="cv-ac-icon-wrap"><i class="fas {{ $data->package_tab_icon ?? 'fa-star' }}"></i></span>
