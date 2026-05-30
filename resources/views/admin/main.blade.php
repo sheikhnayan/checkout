4d92d061e86095e5284f5c84a167cc23aac3e0c2
sheikh Nayan <sheikhnayan1997@gmail.com>
2026-05-30 20:37:54 +0600

    test

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -15,8 +15,10 @@
 
     <meta name="description" content="" />
 
-    <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}" />
+    <!-- Admin favicon: prefer SVG + Safari mask, fallback to ICO -->
+    <link rel="icon" type="image/svg+xml" href="{{ asset('user/assets/img/favicon/favicon.svg') }}" />
+    <link rel="mask-icon" href="{{ asset('user/assets/img/favicon/safari-mask.svg') }}" color="#ffcc00" />
+    <link rel="shortcut icon" href="{{ asset('user/assets/img/favicon/favicon.ico') }}" />
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
