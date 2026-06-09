ac89994d77e736d6ff607d7c3935ea8c3e0cbac8
sheikh Nayan <sheikhnayan1997@gmail.com>
2026-06-09 18:59:31 +0600

    test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8066,14 +8066,14 @@ input[type="checkbox"],
                         '[name="package_year"]'
                     );
                 } else if (stepNumber === 2 && window.requiresTransportation) {
-                    // Validate transportation form
-                    requiredFields.push(
-                        '[name="package_use_date"]',
-                        '[name="transportation_pickup_time"]',
-                        '[name="transportation_address"]',
-                        '[name="transportation_phone"]',
-                        '[name="transportation_guest"]'
-                    );
+                    // Transportation form validation disabled - allow form to proceed
+                    // requiredFields.push(
+                    //     '[name="package_use_date"]',
+                    //     '[name="transportation_pickup_time"]',
+                    //     '[name="transportation_address"]',
+                    //     '[name="transportation_phone"]',
+                    //     '[name="transportation_guest"]'
+                    // );
                 } else if (stepNumber === 2 && !window.requiresTransportation) {
                     // Validate transportation confirmation checkbox
                     if (!$('#transportation_part').is(':checked')) {
