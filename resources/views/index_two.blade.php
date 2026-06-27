aca949ccf63ae1d4113733b452e1d274fd41940e
sheikh Nayan <sheikhnayan1997@gmail.com>
2026-06-27 16:01:59 +0600

    test

--- resources/views/index_two.blade.php
+++ resources/views/index_two.blade.php
@@ -641,7 +641,7 @@
                 transform: translateY(-50%) !important;
                 width: 16px !important;
                 height: 16px !important;
-                background: url('data:image/svg+xml;charset=UTF-8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>') no-repeat center !important;
+                background: url('data:image/svg+xml;charset=UTF-8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>') no-repeat center !important;
                 background-size: contain !important;
                 cursor: pointer !important;
                 z-index: 2 !important;
