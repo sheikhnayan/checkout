31ec38d30937ea4dee7d16071296a3ea93aa24e8
sheikhnayan <sheikhnayan1997@gmail.com>
2026-08-08 04:27:03 +0600

    test

--- resources/views/checkout_templates/template2/index.blade.php
+++ resources/views/checkout_templates/template2/index.blade.php
@@ -7507,6 +7507,70 @@ body.embed-checkout-mode #cv-cart-toast .cv-toast-close {
                 return fields;
             }
 
+            function getCapturedFormFields() {
+                var fields = {};
+
+                // 1. Reservation Date
+                var useDate = $('#package_use_date').val() || $('input[name="package_use_date"]').val() || $('.package_use_date').val() || '';
+                if (useDate) fields.package_use_date = useDate;
+
+                // 2. Transportation & Arrival Details
+                var pickupTime = $('#Pick-up-time').val() || $('input[name="transportation_pickup_time"]').val() || '';
+                if (pickupTime) fields.transportation_pickup_time = pickupTime;
+
+                var pickupAddress = $('#address').val() || $('input[name="transportation_address"]').val() || '';
+                if (pickupAddress) fields.transportation_address = pickupAddress;
+
+                var arrivalTime = $('#Arrival-time').val() || $('input[name="transportation_arrival_time"]').val() || '';
+                if (arrivalTime) fields.transportation_arrival_time = arrivalTime;
+
+                var destination = $('#destination').val() || $('input[name="transportation_destination"]').val() || '';
+                if (destination) fields.transportation_destination = destination;
+
+                // 3. Customer Contact & Personal Information (including prefixed field names)
+                var fieldNames = [
+                    'first_name', 'package_first_name', 'reservation_first_name', 'payment_first_name',
+                    'last_name', 'package_last_name', 'reservation_last_name', 'payment_last_name',
+                    'name',
+                    'email', 'package_email', 'reservation_email', 'payment_email',
+                    'phone', 'package_phone', 'reservation_phone', 'payment_phone',
+                    'dob_month', 'package_dob_month', 'reservation_dob_month', 'payment_dob_month',
+                    'dob_day', 'package_dob_day', 'reservation_dob_day', 'payment_dob_day',
+                    'dob_year', 'package_dob_year', 'reservation_dob_year', 'payment_dob_year',
+                    'dob', 'package_dob', 'reservation_dob', 'payment_dob',
+                    'gender', 'package_gender', 'reservation_gender',
+                    'country', 'package_country', 'reservation_country',
+                    'state', 'package_state', 'reservation_state', 'st-pv', 'state_province',
+                    'city', 'package_city', 'reservation_city',
+                    'zip', 'package_zip', 'reservation_zip', 'postal_code',
+                    'hotel_staying', 'package_hotel_staying', 'reservation_hotel_staying', 'hotel',
+                    'notes', 'package_notes', 'reservation_notes', 'special_requests',
+                    'business_company', 'business_vat', 'business_address'
+                ];
+
+                fieldNames.forEach(function(n) {
+                    var el = $('[name="' + n + '"], #' + n);
+                    if (el.length && el.val()) {
+                        fields[n] = el.val();
+                    }
+                });
+
+                if ($('#businessExpenseCheckbox').length) {
+                    fields.businessExpenseCheckbox = $('#businessExpenseCheckbox').is(':checked');
+                }
+
+                // Custom Form Fields
+                $('[name^="custom_fields"]').each(function() {
+                    var name = $(this).attr('name');
+                    var val = $(this).val();
+                    if (name && val) {
+                        fields[name] = val;
+                    }
+                });
+
+                return fields;
+            }
+
             function getCurrentSelections() {
                 var cartData = window.cart || (typeof cart !== 'undefined' ? cart : []);
                 var couponCode = window.cartCoupon ? window.cartCoupon.code : (typeof cartCoupon !== 'undefined' && cartCoupon ? cartCoupon.code : '');
@@ -7611,6 +7675,34 @@ body.embed-checkout-mode #cv-cart-toast .cv-toast-close {
                                 }
                             });
 
+                            // Email Aliases Sync (email, package_email, reservation_email, payment_email, #email, #hidden_payment_email)
+                            var emailVal = formFields.email || formFields.package_email || formFields.reservation_email || formFields.payment_email || '';
+                            if (emailVal) {
+                                $('[name="email"], [name="package_email"], [name="reservation_email"], [name="payment_email"], #email, #hidden_payment_email')
+                                    .val(emailVal).trigger('change').trigger('input');
+                            }
+
+                            // First Name Aliases Sync
+                            var firstNameVal = formFields.first_name || formFields.package_first_name || formFields.reservation_first_name || formFields.payment_first_name || formFields.name || '';
+                            if (firstNameVal) {
+                                $('[name="first_name"], [name="package_first_name"], [name="reservation_first_name"], [name="payment_first_name"], [name="name"], #first_name')
+                                    .val(firstNameVal).trigger('change').trigger('input');
+                            }
+
+                            // Last Name Aliases Sync
+                            var lastNameVal = formFields.last_name || formFields.package_last_name || formFields.reservation_last_name || formFields.payment_last_name || '';
+                            if (lastNameVal) {
+                                $('[name="last_name"], [name="package_last_name"], [name="reservation_last_name"], [name="payment_last_name"], #last_name')
+                                    .val(lastNameVal).trigger('change').trigger('input');
+                            }
+
+                            // Phone Aliases Sync
+                            var phoneVal = formFields.phone || formFields.package_phone || formFields.reservation_phone || formFields.payment_phone || '';
+                            if (phoneVal) {
+                                $('[name="phone"], [name="package_phone"], [name="reservation_phone"], [name="payment_phone"], #phone, #hidden_payment_phone')
+                                    .val(phoneVal).trigger('change').trigger('input');
+                            }
+
                             // Special handling for package_use_date
                             if (formFields.package_use_date) {
                                 var dateEl = $('#package_use_date, input[name="package_use_date"], .package_use_date');
@@ -7645,6 +7737,8 @@ body.embed-checkout-mode #cv-cart-toast .cv-toast-close {
                     console.error('Error in setSelectionsFromParams:', e);
                 }
             }
+
+            
 
             
 
