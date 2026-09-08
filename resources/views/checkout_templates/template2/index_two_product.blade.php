923a35f980d0754be36be5cf802e1fe567d35d9c
sheikhnayan <sheikhnayan1997@gmail.com>
2026-09-09 01:52:42 +0600

    test

--- resources/views/checkout_templates/template2/index_two_product.blade.php
+++ resources/views/checkout_templates/template2/index_two_product.blade.php
@@ -7102,7 +7102,7 @@
             </div>{{-- end cv-main-col --}}
 
             {{-- RIGHT: Order Summary Sidebar --}}
-            <aside class="cv-sidebar" id="cv-order-sidebar" style="width: 100% !important;">
+            <aside class="cv-sidebar" id="cv-order-sidebar">
                 <div class="cv-sidebar-header">
                     <span>ORDER SUMMARY</span>
                     {{-- <button type="button" class="cv-sidebar-edit-btn" id="cv-edit-cart" style="display:none;"><i class="fas fa-pen"></i> Edit Cart</button> --}}
