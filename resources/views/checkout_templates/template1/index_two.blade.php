202457d3adacf069bb254d8128a9cef13abf4b00
sheikhnayan <sheikhnayan1997@gmail.com>
2026-09-19 10:34:41 +0600

    Fix flatpickr calendar month selector cutoff and year input visibility

--- resources/views/checkout_templates/template1/index_two.blade.php
+++ resources/views/checkout_templates/template1/index_two.blade.php
@@ -22,6 +22,7 @@
             integrity="sha512-fw7f+TcMjTb7bpbLJZlP8g2Y4XcCyFZW8uy8HsRZsH/SwbMw0plKHFHr99DN3l04VsYNwvzicUX/6qurvIxbxw=="
             crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
         <link rel="stylesheet" href="{{ asset('styles/main.css') }}">
         
         @php
